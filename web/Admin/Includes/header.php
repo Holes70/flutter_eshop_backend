5e9289458a245ed5de6ed8b116c6eac7b268b750
Bombarder70 <holespato@gmail.com>
2022-01-31 14:39:17 +0100

    Header update

--- web/Admin/Includes/header.php
+++ web/Admin/Includes/header.php
@@ -90,44 +90,11 @@
       <ul class='list-unstyled components'>
         {$navigationHTML}
       </ul>
-      <ul class='list-unstyled CTAs'>
-        <li>
-          <a href='systemove-nastavenia' class='system-settings'>Systémové nastavenia</a>
-        </li>
-      </ul>
     </nav>
     <div id='content'>
       <nav class='navbar navbar-expand-lg navbar-light bg-white'>
-        <div class='container-fluid'>
-          <button type='button' id='sidebarCollapse' class='btn btn-primary'>
-            <i class='fas fa-arrow-left color-secondary'></i>
-          </button>
-          <button class='btn btn-dark d-inline-block d-lg-none ml-auto' type='button' data-toggle='collapse' data-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
-            <i class='fas fa-align-justify'></i>
-          </button>
-          <div class='ml-3'>
-            {$elasticSearch->show()}
-          </div>
-          <div class='collapse navbar-collapse' id='navbarSupportedContent'>
-            <ul class='nav navbar-nav ml-auto'>
-              <li class='nav-item'>
-                <a href=''>
-                  <i class='fas fa-comments'></i>
-                </a>
-              </li>
-              <li class='nav-item'>
-                <a href=''>
-                  <i class='fas fa-envelope'></i>
-                </a>
-              </li>
-              <li class='nav-item pl-4 pr-2'>
-                <div class='divider-v'></div>
-              </li>
-              <li class='nav-item'>
-                {$profileCard->show()}
-              </li>
-            </ul>
-          </div>
+        <div class='container-fluid' style='color:grey;height:38px'>
+          This is demo version of CMS Dia
         </div>
       </nav>
       {$memory->show()}
